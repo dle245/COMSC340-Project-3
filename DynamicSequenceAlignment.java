1c7eefd939152509ad55401391dc0f4e9c7f3e53
dle245 <120441525+dle245@users.noreply.github.com>
2023-04-25 01:17:23 -0400

    Update DynamicSequenceAlignment.java

--- DynamicSequenceAlignment.java
+++ DynamicSequenceAlignment.java
@@ -1,32 +1,34 @@
 public class DynamicSequenceAlignment {
 
-	static String[] m = {"A","A","C","A","G","T","T","A","C","C","-"};
-	static String[] n = {"T","A","A","G","G","T","C","A","-"};
+	static String[] x = {"A","A","C","A","G","T","T","A","C","C","-"}; 
+	static String[] y = {"T","A","A","G","G","T","C","A","-"};
+	static int m = x.length;
+	static int n = y.length;
 	
-	public static void main(String[] args) throws Exception {
+	public static void main(String[] args) throws Exception { //Main method
         int[][] example;
-        example = optimalCost(0,0);
-        for (int i = 0; i < m.length; i++) {
-        	for (int j = 0; j < n.length; j++) {
+        example = optimalCost();
+        for (int i = 0; i < m+1; i++) { //Prints out the result
+        	for (int j = 0; j < n+1; j++) {
         		System.out.print(example[i][j] + " ");
         	}
         	System.out.println();
         }
     }
 	
-	public static int[][] optimalCost(int i, int j) {
-		int[][] matrix = new int[m.length+1][n.length+1];
+	public static int[][] optimalCost() {
+		int[][] matrix = new int[m+1][n+1];
 		
-		for(int k = m.length; k > 0; k--) {
-			for (int l = n.length; l > 0; l--) {
-				if (k == 10) {
-					matrix[k][l] = equality2(i,j);
+		for(int i = m; i > 0; i--) {
+			for (int j = n; j > 0; j--) {
+				if (i == m) {
+					matrix[i][j] = checkRight(i,j);
 				}
-				if (l == 8) {
-					matrix[k][l] = equality3(i,j);
+				if (j == n) {
+					matrix[i][j] = checkBottom(i,j);
 				}
 				else {
-					matrix[k][l] = equality1(i,j);
+					matrix[i][j] = checkElse(i,j, matrix);
 				}
 			}
 		}
@@ -35,29 +37,27 @@ public class DynamicSequenceAlignment {
 		
 	}
 	
-	public static int equality1(int i, int j){
-        int penalty = 0;
-        i = m.length;
-        j = n.length;
-        int result = 0;
-        while (i != m.length || j != n.length) {
-        	int sum1 = (i + 1 + j + 1) + penalty;
-        	int sum2 = i+1 + j + 2;
-        	int sum3 = i + j +1 + 2;
-        	result = result + min_cost(sum1, sum2, sum3);
-        	i++;
-        	j++;
-        }
-        return result;  
+	public static int checkRight(int i, int j) { //Checks the right most column
+		return 2*(n-j);
+	}
+	
+	public static int checkBottom(int i, int j) { //Checks the bottom most row
+		return 2*(m-i);
+	}
+	
+	public static int checkElse(int i, int j, int[][] matrix){ //Checks everything else
+		int penalty;
+		if (x[i-1]==y[j-1]) {
+			penalty = 0;
+		}
+		else {
+			penalty = 1;
+		}
+        int sum1 = matrix[i][j] + penalty;
+    	int sum2 = matrix[i][j+1] + 2;
+    	int sum3 = matrix[i][j] + 2;
+        return min_cost(sum1, sum2, sum3);  
     }
-	
-	public static int equality2(int i, int j) {
-		return 2*(m.length-j);
-	}
-	
-	public static int equality3(int i, int j) {
-		return 2*(n.length-i);
-	}
 	
 	static int min_cost(int a, int b, int c){ //Exists because Math.min() doesn't compare 3 variables
         if (a<=b & a<=c){
